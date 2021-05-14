0b7d91fe7095293e892fc6721c25502959b88689
Liew Xi Xin <20019652@myrp.edu.sg>
2021-05-14 21:44:06 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20019652/l04_reservation/MainActivity.java
@@ -0,0 +1,17 @@
+package sg.edu.rp.c346.id20019652.l04_reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
